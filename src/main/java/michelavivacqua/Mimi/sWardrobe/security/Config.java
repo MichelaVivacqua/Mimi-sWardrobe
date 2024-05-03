f79290faa9c29ca38fb45b38a991a282357ad5e9
MichelaVivacqua <vivacquamichela@gmail.com>
2024-05-03 12:42:20 +0200

    configuration cors

--- src/main/java/michelavivacqua/Mimi/sWardrobe/security/Config.java
+++ src/main/java/michelavivacqua/Mimi/sWardrobe/security/Config.java
@@ -9,6 +9,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.config.Customizer;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -19,8 +25,11 @@ public class Config {
         httpSecurity.formLogin(http -> http.disable()); // disabilito il form di login in frontend predefinito (avremo React per quello)
         httpSecurity.csrf(http -> http.disable()); // Non voglio la protezione da CSRF (per l'applicazione media non è necessaria e complicherebbe tutta la faccenda, anche lato FE)
         httpSecurity.sessionManagement(http -> http.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // Non voglio le sessioni (perché utilizzeremo la token based authentication con JWT)
-
+        httpSecurity.cors(Customizer.withDefaults());
         // Possiamo aggiungere dei filtri custom
+
+        // Possiamo decidere se debba essere necessaria o meno un'autenticazione per accedere agli endpoint
+        httpSecurity.authorizeHttpRequests(http -> http.requestMatchers("/**").permitAll());
 
         // Aggiungere/Rimuovere determinate regole di protezione per gli endpoint
         // Possiamo decidere se debba essere necessaria o meno un'autenticazione per accedere agli endpoint
@@ -32,4 +41,20 @@ public class Config {
     @Bean
     PasswordEncoder getBCrypt(){
         return new BCryptPasswordEncoder(11);}
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5174"));
+        config.setAllowedMethods(Arrays.asList("*"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        // Registro la configurazione CORS appena fatta a livello globale su tutti gli endpoint del mio server
+
+        return source;
+
+    }
 }
