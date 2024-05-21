3fa2f865505bbda6706a2b6fa15e6661496fc565
MichelaVivacqua <vivacquamichela@gmail.com>
2024-05-21 16:20:24 +0200

    Update MailgunSender.java

--- src/main/java/michelavivacqua/Mimi/sWardrobe/tools/MailgunSender.java
+++ src/main/java/michelavivacqua/Mimi/sWardrobe/tools/MailgunSender.java
@@ -23,7 +23,17 @@ public class MailgunSender {
                 .queryString("from", "vivacquamichela@gmail.com")
                 .queryString("to", recipient.getEmail())
                 .queryString("subject", "Registrazione Completata!")
-                .queryString("text", "Grazie " + recipient.getName() + " per esserti registrato! Adesso saprai sempre in anticipo cosa indossare, e farai sempre un figurone")
+                .queryString("text", "Grazie " + recipient.getName() + " per esserti registrato! Adesso saprai sempre in anticipo cosa indossare, e farai sempre un figurone!" +
+                        "Mimi's Wardrobe è l'app che risponderà tutte le volte alla tua domanda: ❔\"Che cosa indosso oggi?\"❔\n" +
+                        "Registra i tuoi indumenti ed i tuoi accessori, consultali, filtrali!" +
+                        "Crea i tuoi outfit, ordinali per occasione, segna quando li indossi,\n" +
+                        "e sfrutterai al meglio ogni angolo del tuo armadio! " +
+                        "Mimi's Wardrobe svolge al posto tuo il noioso compito di preparare la valigia, così, per la tua vacanza, dovrai pensare solo a divertirti." +
+                        "Mimi's Wardrobe ti fornisce le informazioni meteo di dove andrai, e ti consiglia cosa indossare in base alla temperatura!" +
+                        "Che aspetti?! Vuoi un'altra volta arrivare in ritardo all'aperitivo perchè sei davanti all'armadio ?" +
+                        "Mantieni anche tu il tuo armadio a portata di app")
+
+
                 .asJson();
 
         System.out.println(response.getBody());
